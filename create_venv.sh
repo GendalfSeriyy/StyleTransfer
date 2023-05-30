cfb108345c1858fe41135cdc28a7404b0415c1dc
Gendalf <krivorotov.ia@phystech.edu>
2023-05-30 13:04:59 +0000

    change venv creation

---
+++ create_venv.sh
@@ -0,0 +1,5 @@
+python3.8 -m venv "$(pwd)/venv_styletransfer"
+source venv_styletransfer/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade setuptools
+pip3 install -r requirements.txt
